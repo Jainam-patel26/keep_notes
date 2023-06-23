608d478cd8b22ece4773e6e36ebaf7f1f6ca6632
Jainam-patel26 <pateljainam25@gmail.com>
2023-06-23 15:12:55 +0530

    complete project

--- src/keep_notes_assets/src/components/App.jsx
+++ src/keep_notes_assets/src/components/App.jsx
@@ -3,17 +3,30 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import { useEffect } from "react";
+import { keep_notes } from "../../../declarations/keep_notes/index";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
   function addNote(newNote) {
     setNotes(prevNotes => {
+      keep_notes.createNote(newNote.title, newNote.content)
       return [...prevNotes, newNote];
     });
   }
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData(){
+    const notesArray = await keep_notes.readNotes();
+    setNotes(notesArray);
+  }
+
   function deleteNote(id) {
+    keep_notes.removeNote(id);
     setNotes(prevNotes => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
